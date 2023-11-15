bd97651ae00433e8f215e1ce27da74de906487d2
rasa98 <pantrasa@gmail.com>
2023-11-15 22:37:07 +0100

    widened heuristic

--- src/othello/models/min_max.clj
+++ src/othello/models/min_max.clj
@@ -1,8 +1,30 @@
 (ns othello.models.min-max
-  (:require [othello.core :as game]))
+  (:require [othello.core :as game]
+            [clojure.set :refer [intersection union difference]]))
+
+(def corners #{[0 0] [0 7] [7 0] [7 7]})
+
+(def danger-fields #{[0 1] [1 0] [1 1]
+                     [0 6] [1 6] [1 7]
+                     [6 0] [6 1] [7 1]
+                     [6 6] [6 7] [7 6]})
+
+(def good-to-play #{[2 2] [2 3] [2 4] [2 5]
+                    [3 2]             [3 5]
+                    [4 2]             [4 5]
+                    [5 2] [5 3] [5 4] [5 5]})
 
 (defn my-heuristic [{:keys [white black] :as position-state}]
-  (- (count white) (count black)))
+  (let [all-played (union white black)
+        early-game-factor (if (seq (difference good-to-play all-played)) 3 0)]
+   (+
+     (* 15 (- (count (intersection white corners))
+              (count (intersection black corners))))
+     (* (+ 3 early-game-factor) (- (count (intersection black danger-fields))
+                                   (count (intersection white danger-fields))))
+     (* early-game-factor (- (count (intersection white good-to-play))
+                             (count (intersection black good-to-play))))
+     (- (count white) (count black)))))
 
 (declare minimax)
 
@@ -21,10 +43,7 @@
                 (do
                   (swap! game/abc inc)
                   (reduced {:res new-max-res}))
-                (-> agg
-                    (assoc :a a)
-                    (assoc :res new-max-res)
-                    (assoc :best-paths fields)))))]
+                (assoc agg :a a :res new-max-res :best-paths fields))))]
     (reduce f {:a a :b b :res Double/NEGATIVE_INFINITY :best-paths []} valid-fields)))
 
 (defn minimize [state a b depth valid-fields]
@@ -40,10 +59,7 @@
                 (do
                   (swap! game/abc inc)
                   (reduced {:res new-min-res}))
-                (-> agg
-                    (assoc :b b)
-                    (assoc :res new-min-res)
-                    (assoc :best-paths fields)))))]
+                (assoc agg :b b :res new-min-res :best-paths fields))))]
     (reduce f {:a a :b b :res Double/POSITIVE_INFINITY :best-paths []} valid-fields)))
 
 (defn minimax
@@ -61,7 +77,6 @@
                                                  (> white black) 100000
                                                  :else 0))
                    (= depth 0) (hash-map :res (my-heuristic (:played-fields game-state)))
-                   ;(= end :same-player-turn) (minimax depth a b game-state); todo: same-player-move is it good?!?
                    (= turn :white) (maximize game-state
                                              a
                                              b
